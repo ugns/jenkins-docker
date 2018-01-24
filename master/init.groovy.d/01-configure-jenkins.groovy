b1e4810b9128a416dcef0426cfbf1de4c0c1ef98
Jeremy Bouse <jeremy.bouse@sterlingts.com>
2018-01-24 13:02:23 -0500

    add more init

--- master/init.groovy.d/01-configure-jenkins.groovy
+++ master/init.groovy.d/01-configure-jenkins.groovy
@@ -3,11 +3,15 @@ import jenkins.model.*
 def env = System.getenv()
 
 def config = JenkinsLocationConfiguration.get()
-def instance = Jenkins.getInstance()
 
 config.setUrl(env.JENKINS_UI_URL)
 config.setAdminAddress(env.JENKINS_ADMIN)
 config.save()
+
+println("Jenkins URL Set to " + env.JENKINS_UI_URL)
+println("Jenkins Admin Address Set to " + env.JENKINS_ADMIN)
+
+def instance = Jenkins.getInstance()
 
 instance.setNumExecutors(0)
 if(!instance.isQuietingDown()) {
@@ -25,5 +29,14 @@ else {
     println 'Shutdown mode enabled.  Configure Agent Protocols SKIPPED.'
 }
 
-println("Jenkins URL Set to " + env.JENKINS_UI_URL)
-println("Jenkins Admin Address Set to " + env.JENKINS_ADMIN)
+def rule = Jenkins.instance.getExtensionList(jenkins.security.s2m.MasterKillSwitchConfiguration.class)[0].rule
+if(!rule.getMasterKillSwitch()) {
+    rule.setMasterKillSwitch(true)
+    //dismiss the warning because we don't care (cobertura reporting is broken otherwise)
+    Jenkins.instance.getExtensionList(jenkins.security.s2m.MasterKillSwitchWarning.class)[0].disable(true)
+    Jenkins.instance.save()
+    println 'Disabled agent -> master security for cobertura.'
+}
+else {
+    println 'Nothing changed.  Agent -> master security already disabled.'
+}
