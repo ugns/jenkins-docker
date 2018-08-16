f18a4becbd44699cf46d0f1a09896a2a8dfd92bd
Jeremy T. Bouse <Jeremy.Bouse@UnderGrid.net>
2018-08-15 20:12:38 -0400

    Make changes to fix groovy script

--- master/init.groovy.d/configure-jenkins-admin.groovy
+++ master/init.groovy.d/configure-jenkins-admin.groovy
@@ -1,20 +1,23 @@
 import jenkins.model.*
 import hudson.security.*
 
+def desiredRealm = System.getenv('JENKINS_REALM') ?: "PRIVATE"
+def desiredStrategy = System.getenv('JENKINS_STRATEGY') ?: "PROJECT"
 
 Jenkins j = Jenkins.getInstance()
 
-if (!(j.getSecurityRealm())) {
+if (!(j.getSecurityRealm() instanceof HudsonPrivateSecurityRealm) and desiredRealm == "PRIVATE") {
+    println
     j.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
 }
 
-if (!(j.getAuthorizationStrategy())) {
+if (!(j.getAuthorizationStrategy() instanceof ProjectMatrixAuthorizationStrategy) and desiredStrategy == "PROJECT") {
     j.setAuthorizationStrategy(new ProjectMatrixAuthorizationStrategy())
 }
 
 def adminUsername = System.getenv('JENKINS_ADMIN_USERNAME') ?: 'admin'
 def adminPassword = System.getenv('JENKINS_ADMIN_PASSWORD') ?: 'password'
-def currentUsers = jenkins.getSecurityRealm().getAllUsers().collect { it.getId() }
+def currentUsers = j.getSecurityRealm().getAllUsers().collect { it.getId() }
 if (!(adminUsername in currentUsers)) {
     def user = j.getSecurityRealm().createAccount(adminUsername, adminPassword)
     user.save
