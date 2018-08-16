fc7aa7a8596ca980500bd7c4f72fb68082a10c2f
Jeremy Bouse <jeremy.bouse@sterlingts.com>
2018-08-16 11:56:08 -0400

    Update global security settings init script
    * Use flag to determine if changes are made and need to be saved to disk
    * Check if creating admin user is even possible for SecurtyRealms that
    don't provide functionality to do so
    * Check that admin user has ADMINISTER permission

--- master/init.groovy.d/configure-jenkins-admin.groovy
+++ master/init.groovy.d/configure-jenkins-admin.groovy
@@ -2,25 +2,39 @@ import jenkins.model.*
 import hudson.security.*
 
 Jenkins j = Jenkins.getInstance()
+Boolean save = false
 
 if (j.getSecurityRealm().getClass() == SecurityRealm.None) {
     println("Hudson Private Security Realm enabled")
     j.setSecurityRealm(new HudsonPrivateSecurityRealm(false))
+    save = true
 }
 
 if (j.getAuthorizationStrategy().getClass() == AuthorizationStrategy.Unsecured) {
     println("Project Matrix Authorization Strategy enabled")
     j.setAuthorizationStrategy(new ProjectMatrixAuthorizationStrategy())
+    save = true
 }
 
 def adminUsername = System.getenv('JENKINS_ADMIN_USERNAME') ?: 'admin'
 def adminPassword = System.getenv('JENKINS_ADMIN_PASSWORD') ?: 'password'
 def currentUsers = j.getSecurityRealm().getAllUsers().collect { it.getId() }
 if (!(adminUsername in currentUsers)) {
-    println("Created administrative user ${adminUsername}")
-    def user = j.getSecurityRealm().createAccount(adminUsername, adminPassword)
-    user.save
+    if (j.getSecurityRealm().metaClass.respondsTo(j.getSecurityRealm(), 'createAccount', String, String)) {
+        println("Created administrative user ${adminUsername}")
+        def user = j.getSecurityRealm().createAccount(adminUsername, adminPassword)
+        user.save
+    }
+}
+if (!(j.getAuthorizationStrategy().hasPermission(adminUsername, Jenkins.ADMINISTER))) {
     j.getAuthorizationStrategy().add(Jenkins.ADMINISTER, adminUsername)
+    save = true
 }
 
-j.save()
+//save configuration to disk
+if(save) {
+    j.save()
+}
+else {
+    println 'Nothing changed.  Jenkins Global Security settings already configured.'
+}
